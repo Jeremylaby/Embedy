5609d2b162dde2e1d5e12fc3b2ca5bb7172adf81
Jeremy <barycki@student.agh.edu.pl>
2025-04-06 14:45:44 +0200

    dalej nie działa

--- arduino/Radio/Radio.ino
+++ arduino/Radio/Radio.ino
@@ -5,7 +5,7 @@
 #define UPPER_MID 520
 #define LOWER_MID 500
 
-const uint64_t pipeOut = 0xE9E8F0F0E1LL;  // Adres komunikacyjny (musi być zgodny z odbiornikiem)
+const uint64_t pipeOut = 0xd2f0f0f0f0LL;  // Adres komunikacyjny (musi być zgodny z odbiornikiem)
 RF24 radio(7, 8); // CE, CSN piny
 
 struct MotorSignal {
@@ -27,10 +27,10 @@ void setup() {
     Serial.println("Błąd inicjalizacji nRF24L01!");
     while (1); // Zatrzymaj program
   }
+  radio.setChannel(46);
   radio.setAutoAck(false);
   radio.openWritingPipe(pipeOut);      // Adres odbiornika
   radio.stopListening();               // Tryb nadawania
-
   ResetData();
 
   Serial.println("Nadajnik uruchomiony!");
@@ -41,10 +41,10 @@ int parseValue(int val) {
   val = constrain(val, 0, 1023);
 
   if (val >= LOWER_MID && val <= UPPER_MID) {
-    return 127; // Martwa strefa (środek)
+    return 127;
   } else if (val < LOWER_MID) {
     return map(val, 0, LOWER_MID, 0, 127); // Ruch wsteczny
-  } else { // val > UPPER_MID
+  } else {
     return map(val, UPPER_MID, 1023, 128, 255); // Ruch do przodu
   }
 }
@@ -74,5 +74,5 @@ void loop() {
     Serial.println("nRF24L01 nie wykryty!");
   }
 
-  delay(100); // Odstęp między wysyłaniem
+  delay(1000); // Odstęp między wysyłaniem
 }
--- raspberry-tank/radio_receiver.py
+++ raspberry-tank/radio_receiver.py
@@ -1,33 +1,23 @@
-from machine import Pin, SPI, UART
+from machine import Pin, SPI
+from time import ticks_ms, sleep_ms
 from lib.nrf24l01 import NRF24L01
-import utime
 
-# SPI: SCK=GP18, MOSI=GP19, MISO=GP20
-spi = SPI(0, sck=Pin(18), mosi=Pin(19), miso=Pin(20))
-csn = Pin(16, Pin.OUT)
-ce = Pin(17, Pin.OUT)
 
-# Upewnij się, że payload_size zgadza się z nadajnikiem!
-nrf = NRF24L01(spi, csn, ce, payload_size=2)
-# Adres musi być dokładnie taki jak w nadajniku
-nrf.open_rx_pipe(1, b'\xe9\xe8\xf0\xf0\xe1')
+spi = SPI(0, sck=Pin(18), mosi=Pin(19), miso=Pin(16))
+csn = Pin(17, mode=Pin.OUT, value=1)
+ce = Pin(20, mode=Pin.OUT, value=0)
+
+
+nrf = NRF24L01(spi, csn, ce, payload_size=4)
+nrf.open_rx_pipe(1, b"\xd2\xf0\xf0\xf0\xf0")
 nrf.start_listening()
-
-print("Nasłuchuję danych NRF24L01...")
+print("NRF starts listening")
 
 while True:
     if nrf.any():
-        try:
-            buf = nrf.recv()
-            print("✅ Odebrano:", buf)
 
-            if len(buf) == 2:
-                motor1 = buf[0]
-                motor2 = buf[1]
-                print(f"motor1: {motor1}, motor2: {motor2}")
-        except OSError as e:
-            print("Błąd odbioru:", e)
+        buf = nrf.recv()
+        data = buf
+        print(f"Odebrano: {list(data)}")
 
-    else:
-        print("Brak danych...")
-    utime.sleep(0.1)
+    sleep_ms(20)
